c524276327917df808590b6c8447eab7cc6cc584
Nikita <khadsenikita@gmail.com>
2023-09-01 14:19:28 +0530

    first commit

---
+++ question.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1 ,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Languages",
+        options: [
+            "A. Hyper Type Multi Languages",
+            "B. Hyper Text Multiple Languague",
+            "C. Hyper Text Markup Languages",
+            "D. Home Text Multi Languague"
+        ]
+    },
+
+    {
+        numb: 2 ,
+        question: "What is an element that does not have a closing tag called?",
+        answer: "B. Empty element",
+        options: [
+            "A. Tag",
+            "B. Empty element",
+            "C. Used to change the font size",
+            "D. None of above"
+        ]
+    },
+
+    {
+        numb: 3 ,
+        question: "What is the difference between an opening tag and a closing tag?",
+        answer: "B. Closing tag has a / in front",
+        options: [
+            "A. Opening tag has a / in front",
+            "B. Closing tag has a / in front",
+            "C. There is no difference",
+            "D. There is no difference"
+        ]
+    },
+
+    {
+        numb: 4 ,
+        question: "What does View Source do?",
+        answer: "B. Brings up a note pad with the HTML code already used for the site.",
+        options: [
+            "A. Nothing",
+            "B. Brings up a note pad with the HTML code already used for the site.",
+            "C. Opens a new website.",
+            "D. Adds a link to google on the page"
+        ]
+    },
+
+    {
+        numb: 5 ,
+        question: "What should values always be enclosed in?",
+        answer: "A. Quotation marks",
+        options: [
+            "A. Quotation marks",
+            "B. Commas",
+            "C. Parenthesis",
+            "D. Semicolon"
+        ]
+    },
+]
